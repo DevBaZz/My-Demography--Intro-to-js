afae3b5f5edad41f07f5dae676a3dc8fe976c401
BaZz <bazuaye50@gmail.com>
2022-06-24 04:13:22 +0100

    All Code

---
+++ index.js
@@ -0,0 +1,12 @@
+const myName =` Clement Femi Bazuaye`;
+const myHeight = 5.8;
+const myCountry = `Nigeria`;
+
+
+alert(
+    `Hello World!📞, 
+My name is ${myName} 😋 and i'm from ${myCountry} 👨🏿‍🦱.
+I'm ${myHeight} in height.
+
+Nice meeting you`
+)
